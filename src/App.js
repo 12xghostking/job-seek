92994ac3ac6851b9f6a14796bf3076d6320fb6b8
anmoln <sirkinggreat@gmail.com>
2023-10-01 22:03:17 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage.js'
+import SignUp from './components/SignUp/SignUp.js';
+import Login from './components/Login/Login.js';
+import EmpDash from './components/EmployerDash/EmpDash.js';
+import SeekerDashboard from './components/SaDash/SaDash.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<SignUp />} />
+     <Route path="/login" element={<Login />} />
+      <Route path="/employer" element={<EmpDash />} />
+       <Route path="/seeker" element={<SeekerDashboard />} />
+   </Routes>
   );
 }
 
